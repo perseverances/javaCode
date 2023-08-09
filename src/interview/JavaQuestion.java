33034d3e09a03c9d5fee216ed1a982719948ac1d
tao.best@foxmail.com <tao.best@foxmail.com>
2023-08-09 14:24:21 +0800

    Java工作相关的问题

---
+++ src/interview/JavaQuestion.java
@@ -0,0 +1,17 @@
+package interview;
+
+/**
+ * @ClassName: JavaQuestion
+ * @Description:
+ * @Author: Per
+ * @Date: 2023/8/9 14:12
+ */
+public class JavaQuestion {
+
+    /**
+     * redis相关问题    ----    redsi 6之后的版本
+     */
+    //redis的5大基本数据类型、4种新类型
+    //NoSql数据库
+    //解决功能性问题、解决扩展性问题（框架--可扩展性）、解决性能性问题（用户量增加）
+}
